5742e9352c60e19ef37061925a18f2254960b6f7
Simar Ghumman <ghumman.simar@gmail.com>
2024-04-04 18:01:10 -0700

    Working Site with Signup, Notes, Image

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "backendecommerce": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "backendecommerce8c6dfca6": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3afd047b1": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
