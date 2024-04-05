651d938da2cd3d12104bbabe4c7ba2fc93d505d6
Simar Ghumman <ghumman.simar@gmail.com>
2024-04-04 17:25:21 -0700

    Final Inital Update

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "backendecommerce8c6dfca6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
